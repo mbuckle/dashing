08f61e830cac149afc3663a90f3c4060e78a07b4
Buckle <matthew.r.buckle@gmail.com>
2016-01-18 23:01:52 -0800

    modified frequency of jobs

--- jobs/buzzwords.rb
+++ jobs/buzzwords.rb
@@ -1,7 +1,7 @@
 buzzwords = ['Paradigm shift', 'Leverage', 'Pivoting', 'Turn-key', 'Streamlininess', 'Exit strategy', 'Synergy', 'Enterprise', 'Web 2.0'] 
 buzzword_counts = Hash.new({ value: 0 })
 
-SCHEDULER.every '2s' do
+SCHEDULER.every '10m' do
   random_buzzword = buzzwords.sample
   buzzword_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
   
